dfc2f41d938050b7380f697d07d2f2e95a7f4534
unknown <bhavikjob@gmail.com>
2021-06-07 13:19:39 +0530

    Routing implimentation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import HomePage from './page/homepage/homepage.component';
+import {Switch,Route} from "react-router-dom";
+const Hats=()=>(
+  <div>
+    <h1> Display Hats</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-        <HomePage/>
+      
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/hats' component={Hats}/> 
+      
     </div>
   );
 }
--- src/page/homepage/homepage.component.jsx
+++ src/page/homepage/homepage.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './homepage.styles.scss';
 import Directory from '../../component/directory/directory.component';
 
+
 const HomePage = () => (
   <div className='homepage'>
-    <h1>Welcome to my Homepage</h1>
     <Directory/>
   </div>
 );
